e42066c142322fb6852659905653a91cb455552e
al5hn <arthur4it@gmail.com>
2014-09-28 12:55:24 +0800

    Create 2886LookAndSay.cpp
    The first time performed acmProblem by myself.

---
+++ 2886LookAndSay.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int linesum;
+    string linenumber;
+    cin>>linesum;
+    for(int i=0;i<linesum;i++)
+    {
+        cin>>linenumber;
+        string linechar;
+        int charcount[1000]={0};
+        int linelen;
+        linelen=linenumber.length();
+        int cursor=-1;
+        for(int j=0;j<linelen;j++)
+        {
+            if(linechar[cursor]!=linenumber[j])
+            {
+                cursor++;
+                linechar[cursor]=linenumber[j];
+                charcount[cursor]=1;
+            }
+            else
+                charcount[cursor]++;
+        }
+        for(int k=0;k<=cursor;k++)
+        {
+            cout<<charcount[k]<<linechar[k];
+        }
+        cout<<endl;
+    }
+}
