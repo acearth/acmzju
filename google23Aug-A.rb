012566ab9be4dafee748ac64a24c413c1ea6c822
Arthur <arthur4it@gmail.com>
2016-11-03 17:02:07 +0800

    Update google23Aug-A.rb

--- google23Aug-A.rb
+++ google23Aug-A.rb
@@ -1,7 +1,9 @@
 =begin
-Problem
 
-A "0/1 string" is a string in which every character is either 0 or 1. There are two operations that can be performed on a 0/1 string:
+Problem Description
+
+A "0/1 string" is a string in which every character is either 0 or 1. 
+There are two operations that can be performed on a 0/1 string:
 
 switch: Every 0 becomes 1 and every 1 becomes 0. For example, "100" becomes "011".
 reverse: The string is reversed. For example, "100" becomes "001".
@@ -21,7 +23,7 @@ S4 = "001001100011011"
 
 SN = SN-1 + "0" + switch(reverse(SN-1)).
 
-You need to figure out the Kth character of Sgoogol, where googol = 10100.
+You need to figure out the Kth character of Sgoogol, where googol = 10**100(i.e. 10E100).
 
 Input
 
@@ -39,42 +41,39 @@ Small dataset
 1 ≤ K ≤ 105.
 Large dataset
 
-1 ≤ K ≤ 1018.
+1 ≤ K ≤ 10**18.
 Sample
 
 
 Input 
- 	
-Output 
- 
 4
 1
 2
 3
 10
 
+Output 
 Case #1: 0
 Case #2: 0
 Case #3: 1
 Case #4: 0
+
 =end
 
-def getResult
-  resultSeq=[1]
-  66.times {  resultSeq<<resultSeq[-1]*2 } ## fabunacci Sequence, fab(66) is large enough
-  n=gets.chomp.to_i
-  n.times do |i|
-    k=gets.chomp.to_i
-    puts "Case ##{i+1}: #{getSeq(k, resultSeq)}"
-  end
+# Solution below
+
+resultSeq = [1]
+66.times { resultSeq << resultSeq.last * 2 }  # fabonacci Sequence, fab(66) is large enough
+CENTER_SEQUENCE = resultSeq.freeze
+
+def get_result(input_size = gets.chomp.to_i)
+  input_size.times { |i| puts "Case ##{i+1}: #{find_kth_S_googol(gets.chomp.to_i)}" }
 end
 
-def getSeq(i, centerSeq)
-  return i==3 ?  1:0 if i<4
-  centerSeq.each_with_index do |r, j|
-    return 0 if r==i
-    return 1 - getSeq(centerSeq[j-1]*2-i, centerSeq) if r>i
-  end
+def find_kth_S_googol(k)
+  return k == 3 ?  1 : 0 if k < 4
+  target_index = [*CENTER_SEQUENCE.each_with_index].bsearch { |v, _| v >= k }.last
+  CENTER_SEQUENCE[target_index] == k  ? 0 : 1 - find_kth_S_googol(CENTER_SEQUENCE[target_index - 1] * 2 - k)
 end
 
-getResult
+get_result
